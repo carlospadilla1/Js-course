2362b4b2afabff89b48bc604907ff57bd5bc9f1a
Carlos Padilla <cpadillaortega@gmail.com>
2024-10-21 23:10:29 -0400

    C:/Program Files/Git/

--- js/files/04.js
+++ js/files/04.js
@@ -23,3 +23,45 @@ console.log(order);
 console.log(order.unshift());
 
 //sort
+/*
+
+# Some Methods
+
+- .Tofixed()
+- .ToString()
+- .ToExponential()
+- .ToPrecision()
+
+## Another Methods
+
+- .Number()
+- .parseInt()
+- .parseFloat()
+
+## String Methods
+
+- .length
+- .slice()
+- .toUpper()
+- .toLower()
+- .replace and replaceAll
+- .trimStart() and .trimEnd().
+
+## Search in string
+
+- .indexOf(),
+- .lastIndexOf()
+- .search()
+- .includes
+- .endsWith()
+- .startsWith()
+
+## Arrays
+
+.pop(); Delete or show the last item
+.push(); add item
+.shift(); Delete or show the first item
+.unshift(); Show or add item or to start
+.sort(); to order
+
+*/
