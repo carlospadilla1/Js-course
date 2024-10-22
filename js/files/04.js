05446c6200e6e96a5e613051e664c007b31a0c8a
Carlos Padilla <cpadillaortega@gmail.com>
2024-10-21 22:32:33 -0400

    Add push, pop and shift methods

--- js/files/04.js
+++ js/files/04.js
@@ -1,4 +1,15 @@
 let names = ["Carlos", "Carlos1", "Carlos2"];
-names.pop();
-
 console.log(names);
+names.pop(); //last item deleted
+console.log(names);
+
+//
+let carsModels = ["Yaris", "Aveo", "Autana"];
+console.log(carsModels);
+carsModels.push("Corolla");
+console.log(carsModels);
+
+//
+let numbers = [1, 2, 3, 4, 5];
+console.log(numbers);
+console.log(numbers.shift());
