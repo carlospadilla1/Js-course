65effdc11876e3c106d3933c5da7a23f4a04ec3a
Carlos Padilla <cpadillaortega@gmail.com>
2024-10-21 22:53:29 -0400

    Unshift method added

--- js/files/04.js
+++ js/files/04.js
@@ -12,4 +12,12 @@ console.log(carsModels);
 //
 let numbers = [1, 2, 3, 4, 5];
 console.log(numbers);
-console.log(numbers.shift());
+numbers.shift();
+console.log(numbers);
+
+//
+let order = [4, 3, 2, 1];
+console.log(order);
+order.unshift(5);
+console.log(order);
+console.log(order.unshift());
